5ac8bd0998968beb7ee992b1296758d607fe4d90
Sizboo <benny03.szabo@gmail.com>
2025-04-12 16:19:34 -0500

    basic gcp cloud run workflow up

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let port = env::var("PORT").unwrap_or_else(|_| "8080".to_string());
+    let routes = warp::any().map(|| warp::reply::html("Hello, world!"));
+
+    warp::serve(routes)
+        .run(([0, 0, 0, 0], port.parse().unwrap()))
+        .await;
 }
