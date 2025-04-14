618bb29f0f5de5c97e63b10082c8f99b551b2a02
Benjamin Szabo <benny03.szabo@gmail.com>
2025-04-14 15:21:36 -0500

    stubbed server / working on official connection

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use connection::{ConnReq, connector_client::ConnectorClient};
+
+pub mod connection {
+    tonic::include_proto!("connection");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "https://helpful-serf-server-1016068426296.us-south1.run.app";
+    let mut client = ConnectorClient::connect(addr).await?;
+    
+    let request = tonic::Request::new(ConnReq {
+        ipaddr: 1234,
+        port: 111,
+        info: "lots of cool info".to_string(),
+    });
+    
+    let response = client.send_data(request).await?.into_inner();
+    println!("{:?}", response);
+
+   Ok(()) 
+
 }
