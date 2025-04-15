089e1476c4bff8f566985d3907973888141b287b
Benjamin Szabo <35500456+Sizboo@users.noreply.github.com>
2025-04-15 14:21:32 -0500

    Update main.rs

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,14 +1,26 @@
+use tonic::transport::{Channel, ClientTlsConfig};
 use connection::{ConnReq, connector_client::ConnectorClient};
 
 pub mod connection {
     tonic::include_proto!("connection");
 }
 
+const GCLOUD_URL: &str = "https://helpful-serf-server-1016068426296.us-south1.run.app";
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "https://helpful-serf-server-1016068426296.us-south1.run.app";
-    let mut client = ConnectorClient::connect(addr).await?;
-    
+
+    //tls config
+    //webki roots uses Mozilla's certificate store
+    let tls = ClientTlsConfig::new()
+        .with_webpki_roots()
+        .domain_name("helpful-serf-server-1016068426296.us-south1.run.app");
+
+    let endpoint = Channel::from_static(GCLOUD_URL).tls_config(tls)?
+        .connect().await?;
+
+    let mut client = ConnectorClient::new(endpoint);
+
     let request = tonic::Request::new(ConnReq {
         ipaddr: 1234,
         port: 111,
