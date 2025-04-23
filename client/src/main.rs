759cbf4970de0433bf155bdd322703ffa45c4ba6
lukeheard <lukeheard08@gmail.com>
2025-04-23 22:49:39 +0000

    stubbing out TURN

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,5 +1,7 @@
 mod torrent_client;
 
+use tokio::net::UdpSocket;
+use stunclient::StunClient;
 use std::net::{ToSocketAddrs};
 use std::sync::Arc;
 use torrent_client::TorrentClient;
@@ -25,45 +27,57 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let torrent_client = Arc::new(TorrentClient::new(endpoint).await?);
 
-    loop {
+    let local = UdpSocket::bind("0.0.0.0:0")?; // bind to one socket
 
-        let mut input = String::new();
-        std::io::stdin().read_line(&mut input)?;
-        let client_arc = Arc::clone(&torrent_client);
+    // First STUN server
+    let stun1 = StunClient::new("stun.l.google.com:19302".parse()?);
+    let addr1 = stun1.query_external_address(&local)?;
+    println!("Public address via Google: {}", addr1);
 
-        let command = input.trim();
+    // Second STUN server
+    let stun2 = StunClient::new("stun.stunprotocol.org:3478".parse()?);
+    let addr2 = stun2.query_external_address(&local)?;
+    println!("Public address via stunprotocol: {}", addr2);
 
-        match command {
-            "s" => {
-                println!("Seeding");
-
-                client_arc.advertise().await?;
-
-                let client_arc = Arc::clone(&torrent_client);
-
-                tokio::spawn( async move {
-                    client_arc.seeding().await.unwrap();
-                });
-            }
-            "r" => {
-                println!("Requesting");
-                
-                let file_hash = 12345;
-                
-                let mut peer_list = client_arc.file_request(file_hash).await?;
-                
-                client_arc.get_file_from_peer(peer_list.list.pop().unwrap()).await?;
-            }
-            "q" => {
-                println!("Quitting");
-                break;
-            }
-            _ => {
-                println!("Unknown command: {}", command);
-            }
-
-        }
-    }
+    // loop {
+    //
+    //     let mut input = String::new();
+    //     std::io::stdin().read_line(&mut input)?;
+    //     let client_arc = Arc::clone(&torrent_client);
+    //
+    //     let command = input.trim();
+    //
+    //     match command {
+    //         "s" => {
+    //             println!("Seeding");
+    //
+    //             client_arc.advertise().await?;
+    //
+    //             let client_arc = Arc::clone(&torrent_client);
+    //
+    //             tokio::spawn( async move {
+    //                 client_arc.seeding().await.unwrap();
+    //             });
+    //         }
+    //         "r" => {
+    //             println!("Requesting");
+    //
+    //             let file_hash = 12345;
+    //
+    //             let mut peer_list = client_arc.file_request(file_hash).await?;
+    //
+    //             client_arc.get_file_from_peer(peer_list.list.pop().unwrap()).await?;
+    //         }
+    //         "q" => {
+    //             println!("Quitting");
+    //             break;
+    //         }
+    //         _ => {
+    //             println!("Unknown command: {}", command);
+    //         }
+    //
+    //     }
+    // }
 
 
 Ok(())
