c4cd660a0c55766f4808673a1b5eb41cfa92953b
Benjamin Szabo <benny03.szabo@gmail.com>
2025-04-25 17:02:50 -0500

    build quic client

--- client/src/quic_p2p_sender.rs
+++ client/src/quic_p2p_sender.rs
@@ -1,9 +1,10 @@
-use std::fs;
+use std::{error, fs};
 use std::io::ErrorKind;
 use std::net::Ipv4Addr;
 use std::sync::Arc;
-use quinn::crypto::rustls::QuicServerConfig;
+use quinn::crypto::rustls::{QuicClientConfig, QuicServerConfig};
 use quinn::{Endpoint, TokioRuntime};
+use quinn::Side::Server;
 use rustls::pki_types::{CertificateDer, PrivateKeyDer, PrivatePkcs8KeyDer};
 use crate::server_connection::ServerConnection;
 use crate::torrent_client::{connection, TorrentClient};
@@ -11,13 +12,17 @@ use tokio::net::UdpSocket as TokioUdpSocket;
 use tonic::Request;
 use connection::{CertMessage, PeerId, Cert};
 
-pub struct P2PSender {
+pub struct QuicP2PConn {
     endpoint: Endpoint,
-    private_key: Vec<u8>,
+    private_key: Option<Vec<u8>>,
 }
 
-impl P2PSender {
+impl QuicP2PConn {
 
+    /// init_for_certificate enables a quic client to receive the server's self-signed certificate
+    /// this must be called before a client can retrieve a certificate and make a quic connection
+    /// the sooner this is called the better.
+    /// By convention, the receiving peer is the client and will ideally call this prior to hole punching
     pub(crate) async fn init_for_certificate(
         self, 
         self_addr: PeerId,
@@ -28,14 +33,16 @@ impl P2PSender {
         let request = Request::new(self_addr);
         
         let res = server_connection.init_cert_sender(request).await;
+        println!("Client init certificate {:?}", res);
         eprintln!("Response from init for certificate {:?}", res)
     }
+    
     pub(crate) async fn create_quic_server(
         torrent_client: &mut TorrentClient, 
         socket: TokioUdpSocket, 
         peer_id: PeerId,
         server: ServerConnection
-    ) -> Result<P2PSender, Box<dyn std::error::Error>> {
+    ) -> Result<QuicP2PConn, Box<dyn std::error::Error>> {
         
         let (certs, key) = {
             println!("generating self-signed certificate");
@@ -54,6 +61,7 @@ impl P2PSender {
             peer_id: Some(peer_id),
             cert: Some(Cert { certificate: cert_bytes }),
         });
+        println!("Server sent client certificate!");
         
         
         let res = server_connection.send_cert(request).await?;
@@ -81,11 +89,52 @@ impl P2PSender {
         )?;
 
         Ok(
-            P2PSender {
+            QuicP2PConn {
                 endpoint,
-                private_key: key,
+                private_key: Some(key),
             }
         )
+    }
+    
+    pub (crate) async fn create_quic_client (
+        socket: TokioUdpSocket,
+        self_addr: PeerId,
+        server: ServerConnection,
+    ) -> Result<QuicP2PConn, Box<dyn std::error::Error>> {
+        let mut server_connection = server.client.clone();
+        
+        let request = Request::new(self_addr);
+        let cert_res = server_connection.get_cert(request).await?.into_inner();
+        let cert_bytes = cert_res.certificate;
+        
+        println!("Client received certificate {:?}", cert_bytes);
+        
+        let mut roots = rustls::RootCertStore::empty();
+        
+        roots.add(CertificateDer::try_from(&cert_bytes[..])?).map_err(|e|  Err(Box::new(e)));
+        
+        let mut client_crypto = rustls::ClientConfig::builder()
+            .with_root_certificates(roots)
+            .with_no_client_auth();
+
+        //set crypto with custom protocol type
+        client_crypto.alpn_protocols = vec![b"helpful-serf-p2p".to_vec()];
+        
+        let client_config = quinn::ClientConfig::new(Arc::new(QuicClientConfig::try_from(client_crypto)?));
+
+        let mut endpoint = Endpoint::new(
+            quinn::EndpointConfig::default(),
+            None,
+            socket.into_std().unwrap(),
+            Arc::new(TokioRuntime),
+        )?;
+        
+        endpoint.set_default_client_config(client_config);
+
+        Ok( QuicP2PConn {
+            endpoint,
+            private_key: None,
+        })
     }
     pub(crate) async fn send_data(&self) {
         let conn = self.endpoint.accept().await.unwrap();
